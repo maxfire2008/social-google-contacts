4e0e0594971b64c751927cc7654b40e958c3585b
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-07-11 11:48:23 +1000

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,12 @@
 function updateContactFieldsInViewMode() {
 	var contactItems = document.getElementsByClassName("DRlHd");
+	console.log(contactItems);
 	for(var i = 0; i < contactItems.length; i++) {
 		//custom_field = contactItems[i].getElementsByClassName("JuD4Cc VKOTgd")[0];
 		//console.log(contactItems[i]);
-		if (contactItems.getElementsByClassName("VKOTgd")[0].children[1].textContent.split("• ")[1] == "net.maxstuff.social-google-contacts.mcjava") {
-			contactItems.getElementsByClassName("VKOTgd")[0].children[0].textContent = "asdf";
+		if (contactItems[i].getElementsByClassName("VKOTgd")[0].children[1].textContent == "  • net.maxstuff.social-google-contacts.mcjava") {
+			contactItems[i].getElementsByClassName("VKOTgd")[0].children[0].textContent = "asdf";
 		}
 	}
 }
-document.onload = setTimeout(updateContactFieldsInViewMode(),5000);
+//document.onload = setTimeout(updateContactFieldsInViewMode(),5000);
