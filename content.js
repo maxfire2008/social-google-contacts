582618e2004203b3da70a88f8baa2290a33dbcb1
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-07-11 11:40:54 +1000

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
-document.onload = function () {
+function updateContactFieldsInViewMode() {
 	var contactItems = document.getElementsByClassName("DRlHd");
 	for(var i = 0; i < contactItems.length; i++) {
 		//custom_field = contactItems[i].getElementsByClassName("JuD4Cc VKOTgd")[0];
-		console.log(contactItems[i]);
+		//console.log(contactItems[i]);
+		if (contactItems.getElementsByClassName("VKOTgd")[0].children[1].textContent.split("• ")[1] == "net.maxstuff.social-google-contacts.mcjava") {
+			contactItems.getElementsByClassName("VKOTgd")[0].children[0].textContent = "asdf";
+		}
 	}
 }
+document.onload = setTimeout(updateContactFieldsInViewMode(),5000);
