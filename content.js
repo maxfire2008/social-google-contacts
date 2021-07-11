30a3425562aa9f4246d0f9289d29494174dde28c
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-07-11 11:29:08 +1000

    updated several files

---
+++ content.js
@@ -0,0 +1,7 @@
+document.onload = function () {
+	var contactItems = document.getElementsByClassName("DRlHd");
+	for(var i = 0; i < contactItems.length; i++) {
+		//custom_field = contactItems[i].getElementsByClassName("JuD4Cc VKOTgd")[0];
+		console.log(contactItems[i]);
+	}
+}
